effb5f349898730bd4c83ae9827f73eb24c59898
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-01 09:00:07 +0200

    Update insertVIs.m

--- SPECCHIO-Matlab Code/Helper/insertVIs.m
+++ SPECCHIO-Matlab Code/Helper/insertVIs.m
@@ -9,15 +9,13 @@ function insertVIs(user_data, ids, VIs)
         for j=1:width(VIs)
             
             if(isfinite(VIs{k,j}))
-                mp.setValue(val);
                 mp = MetaParameter.newInstance(user_data.specchio_client.getAttributesNameHash().get(VIs.Properties.VariableNames{j}));
                 val = VIs{k,j};
+                mp.setValue(val);
             else
                 mp = MetaParameter.newInstance(user_data.specchio_client.getAttributesNameHash().get('Garbage Flag'));
                 val = 1;
                 mp. setValue(val);
-                
-                break
             end
 %             disp([ 'Row number = ' num2str(k) ', VI = ' VIs.Properties.VariableNames{j} ' = ' num2str(val) ' and Spectrum = ' num2str(ids.get(k-1)) ])
             user_data.specchio_client.updateOrInsertEavMetadata(mp, new_spectrum_ids);           

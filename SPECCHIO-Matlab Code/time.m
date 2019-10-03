527610ba6869d6d0fb4313b2428342872ab4105d
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-03 10:44:29 +0200

    Update time.m

--- SPECCHIO-Matlab Code/time.m
+++ SPECCHIO-Matlab Code/time.m
@@ -9,7 +9,7 @@ switchedChannels    = true;
 % Process Level 0 (DN) to Level 1 (Radiance)
 % parfor i=1:size(rawDataID,2) % Theoretically works but at some point
 % throws web client exception: returned a response status of 500 Internal Server Error
-for i=1:size(rawDataID)
+for i=2:size(rawDataID,2)
     FLOXBOX_Level_1(rawDataID(i), connectionID, switchedChannels);
 end
 

bd25a76ffc5e00c4bc912d0a3b212ddcd85527d9
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-03 09:58:21 +0200

    Update time.m

--- SPECCHIO-Matlab Code/time.m
+++ SPECCHIO-Matlab Code/time.m
@@ -1,17 +1,20 @@
 %% Start
 % Define Hierarchy Levels:
 rawDataID           = [153, 154, 155];
-radianceDataID      = ;
+radianceDataID      = [157, 156, 158];
 reflectanceDataID   = ;
 connectionID        = 2;
 switchedChannels    = true;
 
 % Process Level 0 (DN) to Level 1 (Radiance)
-parfor i=1:size(rawDataID,2)
+% parfor i=1:size(rawDataID,2) % Theoretically works but at some point
+% throws web client exception: returned a response status of 500 Internal Server Error
+for i=1:size(rawDataID)
     FLOXBOX_Level_1(rawDataID(i), connectionID, switchedChannels);
 end
 
 % Process Level 1 (Radiance) to Level 2 (Reflectance); note that for QEpro
 % it currently stores SIF not the Reflectance.
-FLOXBOX_Level_2(radianceDataID, connectionID, switchedChannels);
-
+for i=1:size(radianceDataID,2)
+    FLOXBOX_Level_2(radianceDataID(i), connectionID, switchedChannels);
+end

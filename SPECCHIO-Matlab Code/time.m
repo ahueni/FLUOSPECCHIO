3c4e9b5f095851ca1b14fc54d0625d8e956780a1
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-03 09:48:10 +0200

    Update time.m

--- SPECCHIO-Matlab Code/time.m
+++ SPECCHIO-Matlab Code/time.m
@@ -1,13 +1,15 @@
 %% Start
 % Define Hierarchy Levels:
-rawDataID           = 81;
-radianceDataID      = 129;
-reflectanceDataID   = 130;
+rawDataID           = [153, 154, 155];
+radianceDataID      = ;
+reflectanceDataID   = ;
 connectionID        = 2;
 switchedChannels    = true;
 
 % Process Level 0 (DN) to Level 1 (Radiance)
-FLOXBOX_Level_1(rawDataID, connectionID, switchedChannels);
+parfor i=1:size(rawDataID,2)
+    FLOXBOX_Level_1(rawDataID(i), connectionID, switchedChannels);
+end
 
 % Process Level 1 (Radiance) to Level 2 (Reflectance); note that for QEpro
 % it currently stores SIF not the Reflectance.

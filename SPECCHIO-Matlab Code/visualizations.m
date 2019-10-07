f55f6dca406fadf6b0ab66810ec17493dc9f0b25
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-07 11:24:28 +0200

    update visualizations

--- SPECCHIO-Matlab Code/visualizations.m
+++ SPECCHIO-Matlab Code/visualizations.m
@@ -88,95 +88,43 @@ hold 'off'
 
 % Define O2B-peak region:
 spectra_QEpro_t         = spectra_QEpro';
-O2B_region              = find(spectra_QEpro_t(wvl_QEpro >= 650 & wvl_QEpro < 687));
+O2B_region              = wvl_QEpro >= 670 & wvl_QEpro < 690;
 O2B_peak                = spectra_QEpro_t == max(spectra_QEpro_t(O2B_region,:),[], 1, 'omitnan');
 
 SpectrumO2B             = nan(size(spectra_QEpro_t));
 SpectrumO2B(O2B_peak)   = spectra_QEpro_t(O2B_peak)';
 
 subplot(2,2,2)
-title('maxF<O2B>')
 for i=1:size(SpectrumO2B,2)
     scatter(wvl_QEpro, SpectrumO2B(:,i), 100)
     hold 'on'
 end
+title('maxF<O2B>')
 hold 'off'
 
 
 % Define O2A-peak region:
-O2A_region              = find(spectra_QEpro_t(wvl_QEpro >= 735));
+O2A_region              = wvl_QEpro >= 735 & wvl_QEpro < 760;
 O2A_peak                = spectra_QEpro_t == max(spectra_QEpro_t(O2A_region,:),[], 1, 'omitnan');
 SpectrumO2A             = nan(size(spectra_QEpro_t));
 SpectrumO2A(O2A_peak)   = spectra_QEpro_t(O2A_peak)';
 
 subplot(2,2,3)
-title('maxF<O2A>')
 for i=1:size(SpectrumO2A,2)
     scatter(wvl_QEpro, SpectrumO2A(:,i), 100)
     hold 'on'
 end
+title('maxF<O2A>')
 hold 'off'
 
-
-
-
-% subplot(2,3,2)
-% plot(wvl_FLAME, spectra_FLAME)
-% axis([640 850 0 1])
-% title('Broadrange')
-
-% subplot(2,3,3)
-% plot(t_QEpro, spectra_QEpro(:,417:670))
-% title('SIF Timeseries @ 720 nm - 760 nm')
-% subplot(2,3,4)
-% plot(t_QEpro, spectra_QEpro(:,118:236))
-% title('SIF Timeseries @ 670 nm - 690 nm')
-
-% Plots , SIF @ 687 and 761
-
-subplot(2,3,1)
-plot(t_QEpro, mean(spectra_QEpro(:, find(wvl_QEpro >= 687 & wvl_QEpro < 688)),2)) % find wavelength, then take mean of the measurements within the given frame mean(A,2) is row mean
-title('SIF Timeseries @ 687 nm')
-
-subplot(2,3,2)
-plot(t_QEpro, mean(spectra_QEpro(:, find(wvl_QEpro >= 760 & wvl_QEpro < 761)),2))
-title('SIF Timeseries @ 760 nm')
-
-% Plots , SIF @ max<F687> and max<761>
-
-subplot(2,3,3)
-plot(wvl_QEpro, nanmax(nanmean(spectra_QEpro(:, find(wvl_QEpro >= 650 & wvl_QEpro < 700)),2),[],2)) 
-title('SIF Timeseries @ 687 nm')
-
-subplot(2,3,4)
-plot(wvl_QEpro,  nanmax(nanmean(spectra_QEpro(:, find(wvl_QEpro >= 700 & wvl_QEpro < 800)),2)),2)
-title('SIF Timeseries @ 760 nm')
-
-
-subplot(2,3,5)
-plot(t_QEpro, ndvi_fl)
-title('NDVI - BROADRANGE')
-
-subplot(2,3,6)
-plot(t_QEpro, evi_fl)
-title('EVI - BROADRANGE')
-
-find(wvl_FLAME<=760)
-find(wvl_FLAME>=761)
+% plot(wvl_QEpro, spectra_QEpro_t(:,50)); % 'Color',jet(193)
+% hold 'on'
+% fill_color = [.929 .694 .125];
+% fh = fill(wvl_QEpro(points), spectra_QEpro_t(points,50),fill_color);
+% hold 'on'
+% fill(wvl_QEpro(end), 0, fill_color)
 % 
-% subplot(2,3,6)
-% plot(t_QEpro, garb_fl)
-% title('Garbage Flagged Points')
-% 
+% trapz(wvl_QEpro, spectra_QEpro_t(:,1), 'omitnan')
 
 
 
-
-% VIsArray = table2array(VIs);
-% figure(2) 
-% clf
-% for i=1:10
-%     subplot(5,2,i)
-%     plot(VIsArray(:,i))
-%     title(VIs.Properties.VariableNames(i))
-% end

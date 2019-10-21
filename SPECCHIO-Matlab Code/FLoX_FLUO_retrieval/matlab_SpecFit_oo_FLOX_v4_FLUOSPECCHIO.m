6f96662de2fbdbd119938834346b86fbe8114bcf
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-21 14:38:06 +0200

    Update matlab_SpecFit_oo_FLOX_v4_FLUOSPECCHIO.m

--- SPECCHIO-Matlab Code/FLoX_FLUO_retrieval/matlab_SpecFit_oo_FLOX_v4_FLUOSPECCHIO.m
+++ SPECCHIO-Matlab Code/FLoX_FLUO_retrieval/matlab_SpecFit_oo_FLOX_v4_FLUOSPECCHIO.m
@@ -91,13 +91,15 @@ L0_filter       = L(500,:)>=0.01;
 %% Spectral subset of input spectra to min_wvl - max_wvl range
 % and convert to mW
 wvlF                    = subset_2d_array(wvl,wvl,wvlRet(1),wvlRet(2));
-Lin_F                   = L0(lb:ub,:)*1e3;
-Lup_F                   = L(lb:ub,:)*1e3;
+Lin                     = L0(lb:ub,:)*1e3;
+Lup                     = L(lb:ub,:)*1e3;
 
+% O2A
 [wvl_A,Lin_A,sub_ind_A] = subset_2d_array(wvl, L0, wvl_def.sfm_low_wl_A, wvl_def.sfm_up_wl_A);
 [~,Lup_A]               = subset_2d_array(wvlF, L, wvl_def.sfm_low_wl_A, wvl_def.sfm_up_wl_A);
 [~,iwvl_A_760]          = min(abs(wvl_A-760));
 
+% O2B
 [wvl_B,Lin_B,sub_ind_B] = subset_2d_array(wvl, L0, wvl_def.sfm_low_wl_B, wvl_def.sfm_up_wl_B);
 [~,Lup_B]               = subset_2d_array(wvlF, L, wvl_def.sfm_low_wl_B, wvl_def.sfm_up_wl_B);
 [~,iwvl_B_687]          = min(abs(wvl_B-687));
@@ -142,24 +144,19 @@ end
 hbar = parfor_progressbar(n_files-1,'Please wait...');
 
 parfor i = 1:n_files
-        
+    % -- process only filtered data
+    if L0_filter(i) == 1
     % Processing
-        
-        %
-        [x_A,f_wvl_A,r_wvl_A,resnorm_A,exitflag_A,output_A,f_FLD_A,r_FLD_A]=NBFret_VPSPLINE...
-            (wvl_A,Lin_A(:,i),Lup_A(:,i),'A',0,w_A,opt_alg,stio);
-        
-        [x_B,f_wvl_B,r_wvl_B,resnorm_B,exitflag_B,output_B,f_FLD_B,r_FLD_B]=NBFret_VPSPLINE...
-            (wvl_B,Lin_B(:,i),Lup_B(:,i),'B',0,w_B,opt_alg,stio);
-        
-        [x_F,f_wvl_F,r_wvl_F,resnorm_F,exitflag_F,output_F]=FLOX_SpecFit_6C(...
-            wvlF,Lin_F(:,i),Lup_F(:,i),[],do_plot,w_F,opt_alg,stio,wvl);
-        
-        out_arr(i,:) = [ ...
-            f_FLD_A r_FLD_A f_wvl_A(iwvl_A_760) r_wvl_A(iwvl_A_760) resnorm_A exitflag_A output_A.iterations ...
-            f_FLD_B r_FLD_B f_wvl_B(iwvl_B_687) r_wvl_A(iwvl_B_687) resnorm_B exitflag_B output_B.iterations ...
-            f_wvl_F(iowvl_760) r_wvl_F(iowvl_760) f_wvl_F(iowvl_687) r_wvl_F(iowvl_687) resnorm_F exitflag_F output_F.iterations ...
-            ];
+    [x_F,f_wvl_F,r_wvl_F,resnorm_F,exitflag_F,output_F] = FLOX_SpecFit_6C...
+        (wvl,Lin(:,i),Lup(:,i),[1,1],w_F,opt_alg,stio,wvl);
+
+    % spectral output from SpecFit    
+        out_arr(i,:) = [f_FLD_A r_FLD_A f_wvl_A(iwvl_A_760) r_wvl_A(iwvl_A_760) ...
+            resnorm_A exitflag_A output_A.iterations f_FLD_B r_FLD_B ...
+            f_wvl_B(iwvl_B_687) r_wvl_A(iwvl_B_687) resnorm_B exitflag_B ...
+            output_B.iterations f_wvl_F(iowvl_760) r_wvl_F(iowvl_760) ...
+            f_wvl_F(iowvl_687) r_wvl_F(iowvl_687) resnorm_F exitflag_F ...
+            output_F.iterations ];
         
         %
         outF_SFM_A(:,i) = f_wvl_A;

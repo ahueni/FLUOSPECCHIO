ba35080bd6d1a4360023fe2f2dc0f36a3b5e0e35
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-25 13:47:35 +0200

    Update singleClientMain.m

--- SPECCHIO-Matlab Code/singleClientMain.m
+++ SPECCHIO-Matlab Code/singleClientMain.m
@@ -37,35 +37,10 @@ user_data.cf              = SPECCHIOClientFactory.getInstance();
 user_data.descriptor_list = user_data.cf.getAllServerDescriptors();
 user_data.specchio_client = user_data.cf.createClient(user_data.descriptor_list.get(connectionID));
 
-% set up a campaign data loader
-user_data.cdl = SpecchioCampaignDataLoader(user_data.specchio_client);
-
-% Get campaign to be loaded by its ID (The campaign ID can be selected in
-% the SPECCHIO Data Browser GUI using the context sensitive menu in the
-% 'matching spectra' field
-user_data.campaign = user_data.specchio_client.getCampaign(49);
-disp(user_data.campaign.getName());
-
-% Add file path
-fileStoragePath = 'C:\Users\bbuman\Documents\GitHub\FLUOSPECCHIO\Example Data\CH-OE2_Oensingen\2019\190611_tinysubset';
-fileName        = '190611_tinysubset';
-user_data.campaign.addKnownPath(fileStoragePath)
-
-% load campaign data
-user_data.cdl.set_campaign(user_data.campaign);
-user_data.cdl.start();
-
-delay = 0.1;  % 10 milliseconds
-while user_data.cdl.isAlive  
-    pause(delay);  % a slight pause before checking again if thread is alive
-end
-
-disp(['Number of parsed files: ' num2str(user_data.cdl.getParsed_file_counter)])
-disp(['Number of inserted files: ' num2str(user_data.cdl.getSuccessful_file_counter)])
-
-
-% wait for loading thread to finish: otherwise the number of parsed and
-% loaded files will be wrong as thread is still working.
+% Load data into db
+filePath = 'C:\Users\bbuman\Documents\GitHub\FLUOSPECCHIO\Example Data\CH-OE2_Oensingen\2019\190611_tinysubset';
+fileName = '190611_tinysubset';
+autoLoadCampaignData(user_data, filePath);
 
 %% Processing L0 -> L1
 % Box setup (Laegeren has switched up-and-downwellling channels):

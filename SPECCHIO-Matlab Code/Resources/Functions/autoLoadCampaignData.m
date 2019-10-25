975523d8c0cabc1979432ea7edab5062bb8ab75b
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-25 13:47:30 +0200

    Create autoLoadCampaignData.m

---
+++ SPECCHIO-Matlab Code/Resources/Functions/autoLoadCampaignData.m
@@ -0,0 +1,27 @@
+function [user_data] = autoLoadCampaignData(user_data, filePath)
+% set up a campaign data loader
+user_data.cdl = SpecchioCampaignDataLoader(user_data.specchio_client);
+
+% Get campaign to be loaded by its ID (The campaign ID can be selected in
+% the SPECCHIO Data Browser GUI using the context sensitive menu in the
+% 'matching spectra' field
+user_data.campaign = user_data.specchio_client.getCampaign(49);
+disp(user_data.campaign.getName());
+
+% Add file path
+user_data.campaign.addKnownPath(filePath)
+
+% load campaign data
+user_data.cdl.set_campaign(user_data.campaign);
+user_data.cdl.start();
+
+delay = 0.1;  % 10 milliseconds
+while user_data.cdl.isAlive
+    pause(delay);  % a slight pause before checking again if thread is alive
+end
+
+disp(['Number of parsed files: ' num2str(user_data.cdl.getParsed_file_counter)])
+disp(['Number of inserted files: ' num2str(user_data.cdl.getSuccessful_file_counter)])
+% wait for loading thread to finish: otherwise the number of parsed and
+% loaded files will be wrong as thread is still working.
+end

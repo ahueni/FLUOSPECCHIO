9020160812354376ded047acf475bb63e0532dc5
bbuman <52099745+bbuman@users.noreply.github.com>
2019-10-31 15:17:50 +0100

    Update singleClientMain.m
    some changes to handle new data inputs

--- SPECCHIO-Matlab Code/singleClientMain.m
+++ SPECCHIO-Matlab Code/singleClientMain.m
@@ -19,7 +19,7 @@
 %   DATE:
 %   12-Sep-2019 V1
 %   23-Oct-2019 V2
-
+tic
 %% Add Path
 addpath(genpath('Resources'));
 
@@ -33,31 +33,32 @@ import ch.specchio.gui.*;
 %% Transfer raw data to DB
 % Define connection
 connectionID    = 3;
+campaignID      = 2;
 
 % Connect
 user_data.cf              = SPECCHIOClientFactory.getInstance();
 user_data.descriptor_list = user_data.cf.getAllServerDescriptors();
 user_data.specchio_client = user_data.cf.createClient(user_data.descriptor_list.get(connectionID));
-user_data.campaign        = user_data.specchio_client.getCampaign(1);
-user_data.topHierarchyId  = user_data.specchio_client.getHierarchyId(user_data.campaign,'1 week', 0);
+user_data.campaign        = user_data.specchio_client.getCampaign(campaignID);
+% user_data.topHierarchyId  = user_data.specchio_client.getHierarchyId(user_data.campaign,'data', 0);
 
 % Load data into db
-filePath = 'C:\Users\bbuman\Downloads\Data\OE2_Oensingen\2019\raw\1 week\190501';
+filePath = 'C:\Users\bbuman\Documents\Data\OE2_Oensingen\2019\raw\1_day\190502';
 fileName = '190502';
-% user_data = autoLoadCampaignData(user_data, 1, filePath);
+user_data = autoLoadCampaignData(user_data, filePath);
 
 %% Processing L0 -> L1
 % Box setup (Laegeren has switched up-and-downwelling channels):
 % Channel switching (true if up and downwelling was switched)
 user_data.switch_channels_for_flox = false;
-user_data.switch_channels_for_rox = false;
+user_data.switch_channels_for_rox  = false;
 
 % Get Hierarchy of the newly loaded file
-rawDataID   = user_data.specchio_client.getSubHierarchyId(fileName, user_data.topHierarchyId);
+rawDataID   = user_data.specchio_client.getSubHierarchyId(fileName, 0);
 node        = hierarchy_node(rawDataID, "", "");
 DN_ids      = user_data.specchio_client.getSpectrumIdsForNode(node);
 % Visualize Raw
-visualizeLevel(user_data, DN_ids, 0);
+% visualizeLevel(user_data, DN_ids, 0);
 % Get spectrum ids:
 
 % Calculate QIs-0
@@ -71,7 +72,7 @@ user_data = processL0ToL1(user_data, DN_ids);
 radianceHierarchy   = user_data.specchio_client.getSubHierarchyId(user_data.campaign, 'Radiance', rawDataID);
 node                = hierarchy_node(radianceHierarchy, "", "");
 radianceDataID      = user_data.specchio_client.getSpectrumIdsForNode(node);
-visualizeLevel(user_data, radianceDataID, 1);
+% visualizeLevel(user_data, radianceDataID, 1);
 %% Process L1 --> L2
 
 % Calculate QIs-1
@@ -80,6 +81,7 @@ visualizeLevel(user_data, radianceDataID, 1);
 
 % Get Reflection 
 user_data = processL1ToL2(user_data, radianceDataID);
+toc
 % Visualize Reflectance
 % Get Hierarchy of the Radiance
 reflectanceHierarchy   = user_data.specchio_client.getSubHierarchyId(user_data.campaign, 'Reflectance', rawDataID);

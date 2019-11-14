c059e2e359b442f6eb7d2fd00783f8a83aa27bb9
bbuman <52099745+bbuman@users.noreply.github.com>
2019-11-14 10:59:01 +0100

    Adding waitbar to the VI insert

--- SPECCHIO-Matlab Code/Resources/Functions/insertVIs.m
+++ SPECCHIO-Matlab Code/Resources/Functions/insertVIs.m
@@ -1,9 +1,13 @@
 function insertVIs(user_data, ids, VIs)
 import ch.specchio.types.*;
     new_spectrum_ids = java.util.ArrayList();
+    f = waitbar(0, 'Insert Index', 'Name', 'VI/QI/TI insert');
     for k=1:height(VIs)
+        waitbar((k/height(VIs)), f, 'Please wait...');
+        
         new_spectrum_ids.clear();
         new_spectrum_ids.add(java.lang.Integer(ids.get(k-1)));
+                
         for j=1:width(VIs)
             
             if(isfinite(VIs{k,j}))
@@ -16,8 +20,8 @@ import ch.specchio.types.*;
                 mp.setValue(val);
             end
 %             disp([ 'Row number = ' num2str(k) ', VI = ' VIs.Properties.VariableNames{j} ' = ' num2str(val) ' and Spectrum = ' num2str(ids.get(k-1)) ])
-            disp('.');
-            user_data.specchio_client.updateOrInsertEavMetadata(mp, new_spectrum_ids);           
+            user_data.specchio_client.updateOrInsertEavMetadata(mp, new_spectrum_ids); 
         end
     end
+    close(f);
 end
